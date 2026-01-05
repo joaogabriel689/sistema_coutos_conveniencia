60bfe133dae11f958b52eda4009bc9b3adb813bb
joao gabriel pereira couto <joaocouto114@gmail.com>
2026-01-04 23:43:50 -0300

    fix database

--- conveniencia/settings.py
+++ conveniencia/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-import dj_database_url
+
 from pathlib import Path
 from decouple import config
 import os
@@ -82,13 +82,13 @@ WSGI_APPLICATION = 'conveniencia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-
-
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
